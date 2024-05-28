db3eb39d1d448c1454c44a8f332c4ac01fbc8716
Atif Zaman <atif.zaman@binogi.com>
2024-05-28 10:37:17 +0200

    Vercel environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  console.log("evn", env);
-  return {
-    plugins: [react()],
-    server: {
-      port: 3030,
-    },
-    define: {
-      __API_URL__: JSON.stringify(env.VITE_API_URL),
-      __APP_ENV__: JSON.stringify(env.VITE_API_KEY),
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3030,
+  },
+  define: {
+    __API_URL__: process.env.VITE_API_URL,
+    __API_KEY__: process.env.VITE_API_KEY,
+  },
 });
